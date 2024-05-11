34bcc8023410aa4bffc8dc63ba64ff8737f34621
AnilHasda <hasdaanil098@gmail.com>
2024-05-11 17:58:07 +0545

    crud for admin category added

--- Backend/Controllers/adminCategoryControlers/adminCategory.js
+++ Backend/Controllers/adminCategoryControlers/adminCategory.js
@@ -1,6 +1,22 @@
 //Admin createCategory controllers
+import { ObjectId } from "mongodb";
 import {model} from "../../Database/adminCategory/Model/categoryModle.js";
 import { validationError,duplicateErrorHandling } from "../../schemaErrorHandling/errorHandling.js";
+//show all the category
+const getCategory=async (req,resp)=>{
+  try{
+  let query=await model.find();
+  if(query.length>0){
+    resp.status(200).json(query);
+  }
+  else{
+    resp.status(200).json({message:"data is not present"});
+  }
+  }catch(error){
+    console.log(error);
+    resp.status(500).json({message:"Internal server error"})
+  }
+}
 //category inserted controller
 const createCategory=async (req,resp)=>{
     console.log(req.body);
@@ -18,4 +34,35 @@ const createCategory=async (req,resp)=>{
           }
     }
 }
-export {createCategory};
+//update controller
+const updateCategory=async (req,resp)=>{
+  let id=req.params.id;
+  try{
+  let query=await model.updateOne({_id:new ObjectId(id)},{$set:{category:req.body.category}});
+  if(query){
+    resp.status(200).json({message:"Data updated successfully"});
+  }
+  else{
+resp.status(500).json({message:"Something went wrong"});
+  }
+}catch(error){
+  console.log(error)
+  resp.status(500).json({message:"Internal server error"});
+}
+}
+const deleteCategory=async (req,resp)=>{
+  let id=req.params.id;
+  try{
+  let query=await model.deleteOne({_id:new ObjectId(id)})
+  if(query){
+    resp.status(200).json({message:"Data deleted successfully"});
+  }
+  else{
+resp.status(500).json({message:"Something went wrong"});
+  }
+  }catch(error){
+    console.log(error)
+  resp.status(500).json({message:"Internal server error"});
+  }
+}
+export {createCategory,updateCategory,deleteCategory,getCategory};
--- Backend/Routes/adminRoute/adminRoute.js
+++ Backend/Routes/adminRoute/adminRoute.js
@@ -1,5 +1,8 @@
 import express from "express";
-import { createCategory } from "../../Controllers/adminCategoryControlers/adminCategory.js";
+import { createCategory, deleteCategory, getCategory, updateCategory } from "../../Controllers/adminCategoryControlers/adminCategory.js";
 let router=express.Router();
 router.post("/admin/createCategory",createCategory);
+router.put("/admin/updateCategory/:id",updateCategory);
+router.delete("/admin/deleteCategory/:id",deleteCategory);
+router.get("/admin/getCategory",getCategory);
 export default router;
